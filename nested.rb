4ba887b4bc943d4ce7cab22ff6812512a053e9ba
karen ercoli <karenercoli@gmail.com>
2018-11-27 16:58:42 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -39,8 +39,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-     
-  programmer_hash[:alan_kay][:known_for] 
+
+  programmer_hash[:alan_kay][:known_for]
 end
 
 alan_kay_is_known_for
@@ -96,10 +96,10 @@ def adding_matz
     languages:
       ["LISP", "C"]
     }
-    
+
   programmer_hash
-    
-    
+
+
 end
 
 adding_matz
@@ -122,7 +122,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-     
+
   programmer_hash[:alan_kay][:known_for] = "GUI"
   programmer_hash
 
